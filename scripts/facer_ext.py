1de3de37fc31c667293f97bc5f8a874b6ff5295c
bbchip0103 <bbchip13@gmail.com>
2023-07-21 18:48:16 +0900

    Update scripts

--- scripts/facer_ext.py
+++ scripts/facer_ext.py
@@ -17,7 +17,7 @@ from modules import devices, lowvram, script_callbacks, shared
 # from pydantic import BaseModel, Field
 
 
-def get_modelname_list(type_='detection'):
+def get_modelnames(type_='detection'):
     if type_.lower()=='detection':
         return ['retinaface/resnet50', 'retinaface/mobilenet']
     elif type_.lower()=='segmentation':
@@ -37,7 +37,7 @@ def image_to_mask(image, mode, seg_model):
 def mount_facer_api(_: gr.Blocks, app: FastAPI):
     @app.get("/facer/models")
     async def get_models(type_):
-        return get_modelname_list(type_)
+        return get_modelnames(type_)
 
 
 def add_tab():
@@ -62,7 +62,7 @@ def single_tab():
             image = gr.Image(type='pil', label="Image")
             with gr.Column():
                 mode = gr.Radio(['hair', 'face', 'neck', 'clothes'], label='Mode', value='best')
-                seg_model = gr.Dropdown(get_models(), value='farl/lapa/448', label='Segmentation model')
+                seg_model = gr.Dropdown(get_modelnames('segmentation'), value='farl/lapa/448', label='Segmentation model')
         mask = gr.Image(type='pil', label="Mask")
         prompt = gr.Textbox(label="Prompt", lines=3)
     with gr.Row():
