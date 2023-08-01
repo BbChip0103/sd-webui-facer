5625142972a357fae7e91124b57ef2e54ba8ce9f
bbchip0103 <bbchip13@gmail.com>
2023-08-01 17:56:32 +0900

    Update API docs

--- scripts/facer_ext.py
+++ scripts/facer_ext.py
@@ -278,7 +278,7 @@ def stringToRGB(base64_string):
     imgdata = base64.b64decode(str(base64_string))
     img = Image.open(io.BytesIO(imgdata))
     opencv_img = cv2.cvtColor(np.array(img), cv2.COLOR_BGR2RGB)
-return opencv_img 
+    return opencv_img 
 
 
 def mount_facer_api(_: gr.Blocks, app: FastAPI):
